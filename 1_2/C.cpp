3f8ae16d11a4e76a6524a768ff2a0a853c8bdfb5
cauchym <detaramewo.iuna@gmail.com>
2018-09-21 06:41:32 +0900

    1_2_C StableSort [p70]

--- 1_2/C.cpp
+++ 1_2/C.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-// using namespace std;
 
 struct Card {char suit, value;};
 
-void bubbleSort(struct Card C[], int N) {
+void printCards(Card C[], int N) {
+    for(int i = 0; i < N; i++) {
+        if (i > 0) {
+            std::cout << " ";
+        }
+        std::cout << C[i].suit << C[i].value;
+    }
+    std::cout << std::endl;
+}
+
+void bubbleSort(Card C[], int N) {
     bool flag = 1;
     int count = 0;
     for (int i = 0; flag; i++) { //未ソート部分の先頭idx
@@ -23,46 +32,53 @@ void bubbleSort(struct Card C[], int N) {
     }
 }
 
-void selectionSort(struct Card C[], int N) {
-
+void selectionSort(Card C[], int N) {
+    int i, j, minj;
+    Card t;
+    for(i = 0; i < N; i++) {
+        minj = i;
+        j = i;
+        for(j = i; j < N; j++) {
+            if (C[j].value < C[minj].value ) {
+                minj = j;
+            }
+        }
+        t = C[i];
+        C[i] = C[minj];
+        C[minj] = t;
+    }
 }
 
-void printCards(struct Card C[], int N) {
-    for(int i; i < N; i++) {
-        if (i > 0) {
-            std::cout << " ";
+bool isStable(Card C1[], Card C2[], int n) {
+    int i;
+    for ( i = 0; i < n; i++ ){
+        if ( C1[i].suit != C2[i].suit) {
+            return false;
         }
-        std::cout << C[i].value;
     }
-    std::cout << std::endl;
-} 
+    return true;
+}
 
 int main() {
     Card Cb[100], Cs[100];
     int n;
     char ch;
     std::cin >> n;
-    for(int i; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         std::cin >> Cb[i].suit >> Cb[i].value;
     }
-    for (int i;i < n;i++) Cs[i] = Cb[i];
-    bubbleSort(Cb, n);
-    // printCards(Cb, n);
-    // printCards(Cs, n);
-
-    for(int j; j < n; j++) {
-        if (j > 0) {
-            std::cout << " ";
-        }
-        std::cout << Cb[j].value;
+    for(int i = 0; i < n; i++) {
+        Cs[i] = Cb[i];
     }
-    std::cout << std::endl;
-
-    for(int k; k < n; k++) {
-        if (k > 0) {
-            std::cout << " ";
-        }
-        std::cout << Cs[k].value;
+    bubbleSort(Cb, n);
+    printCards(Cb, n);
+    std::cout << "Stable" << std::endl;
+    selectionSort(Cs, n);
+    printCards(Cs, n);
+    if ( isStable(Cb, Cs, n) ){
+        std::cout << "Stable" << std::endl;
+    } else {
+        std::cout << "Not stable" << std::endl;
     }
     std::cout << std::endl;
 }
